25af41de7015b29d5d61db278bb33fade12f65dd
hanxianzhai <hanxianzhai@gmail.com>
2019-08-22 23:29:06 +0800

    0.9

--- bin/start.sh
+++ bin/start.sh
@@ -8,21 +8,19 @@ directory_empty() {
 
 if [ ! -d "/opt/davinci"]; then
     cp -a /usr/src/davinci /opt/
-fi
-if [ directory_empty "/opt/davinci"]; then
+elif [ directory_empty "/opt/davinci"]; then
     cp -a /usr/src/davinci/* /opt/davinci/
-fi
-       
-if [ ! -f "/initdb/davinci.sql"]; then
+elif [ ! -f "/initdb/davinci.sql"]; then
     cat /usr/src/davinci/bin/davinci.sql > /initdb/davinci.sql
     sed -i '1i\SET GLOBAL log_bin_trust_function_creators = 1;' /initdb/davinci.sql
+else
+
+    set -e
+
+    host="$1"
+    shift
+    cmd="$@"
 fi
-
-set -e
-
-host="$1"
-shift
-cmd="$@"
 
 until [ $(curl -I -m 10 -o /dev/null -s -w %{size_download} $host) -gt 0 ]; do
   >&2 echo "database is unavailable - sleeping"
